66c918ee477bc995ef796cbe6917fe7826948e05
EgorFokin <egor.fokin2017@gmail.com>
2020-01-12 16:32:22 +0300

    #7

--- AsteriskModule/CallManager.py
+++ AsteriskModule/CallManager.py
@@ -48,28 +48,32 @@ class CallManager:
         except EOFError:
             print("ERROR callManager:connection closed and no cooked data available")
 
-    # def get_channels_status(self):
-    #     try:
-    #         callsDict = []
-    #         self.tn.write(b"sip show channels\n")
-    #         data = self.tn.read_until(b">", 5).splitlines(False)
-    #         for i in range(1, len(data) - 2):
-    #             parts = data[i].split()
-    #             callsDict.append(dict)
-    #             callsDict[i]["Peer1"] = parts[0]
-    #             callsDict[i]["User/ANR"] = parts[1]
-    #             callsDict[i]["Call_ID"] = parts[2]
-    #             callsDict[i]["Format"] = parts[3]
-    #             callsDict[i]["Hold"] = parts[4]
-    #             callsDict[i]["Last_Message"] = parts[5]
-    #             callsDict[i]["Expiry"] = parts[6]
-    #             callsDict[i]["Peer2"] = parts[7]
-    #         return callsDict
-    #
-    #     except EOFError:
-    #         print("ERROR callManager:connection closed and no cooked data available")
-    #     except OSError:
-    #         print("ERROR callManager:connection closed")
+    def get_channels_status(self):
+        try:
+            callsDict = []
+            self.tn.write(b"Action: Command\r\nCommand: sip show channels\r\n\r\n")
+            data = self.tn.read_until(b"END COMMAND", 10).decode().split("\r\n\r\n")
+            for block in data:
+                lines = block.splitlines()
+                print(lines)
+                if lines[0] == 'Response: Follows':
+                    for i in range(3, len(data)-2):
+                        parts = data[i].split()
+                        callsDict.append({})
+                        callsDict[i]["Peer1"] = parts[0]
+                        callsDict[i]["User/ANR"] = parts[1]
+                        callsDict[i]["Call_ID"] = parts[2]
+                        callsDict[i]["Format"] = parts[3]
+                        callsDict[i]["Hold"] = parts[4]
+                        callsDict[i]["Last_Message"] = parts[5]
+                        callsDict[i]["Expiry"] = parts[6]
+                        callsDict[i]["Peer2"] = parts[7]
+                    return callsDict
+
+        except EOFError:
+            print("ERROR callManager:connection closed and no cooked data available")
+        except OSError:
+            print("ERROR callManager:connection closed")
 
     def close(self):
         try:
