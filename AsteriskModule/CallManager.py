a11e1aa71034b71d0e86f9bbb64cac158cd15102
EgorFokin <egor.fokin2017@gmail.com>
2020-01-13 11:04:50 +0300

    #9

--- AsteriskModule/CallManager.py
+++ AsteriskModule/CallManager.py
@@ -54,21 +54,18 @@ class CallManager:
             self.tn.write(b"Action: Command\r\nCommand: sip show channels\r\n\r\n")
             self.tn.read_until(b"Response: Follows\r\n", 10)
             data = self.tn.read_until(b"END COMMAND", 10).decode().split("\r\n\r\n")
-            for block in data:
-                lines = block.splitlines()
-                if lines[0] == 'Privilege: Command':
-                    for i in range(2, len(data)-2):
-                        parts = data[i].split()
-                        callsDict.append({})
-                        callsDict[i]["Peer1"] = parts[0]
-                        callsDict[i]["User/ANR"] = parts[1]
-                        callsDict[i]["Call_ID"] = parts[2]
-                        callsDict[i]["Format"] = parts[3]
-                        callsDict[i]["Hold"] = parts[4]
-                        callsDict[i]["Last_Message"] = parts[5]
-                        callsDict[i]["Expiry"] = parts[6]
-                        callsDict[i]["Peer2"] = parts[7]
-                    return callsDict
+            for i in range(2, len(data)-2):
+                parts = data[i].split()
+                callsDict.append({})
+                callsDict[i]["Peer1"] = parts[0]
+                callsDict[i]["User/ANR"] = parts[1]
+                callsDict[i]["Call_ID"] = parts[2]
+                callsDict[i]["Format"] = parts[3]
+                callsDict[i]["Hold"] = parts[4]
+                callsDict[i]["Last_Message"] = parts[5]
+                callsDict[i]["Expiry"] = parts[6]
+                callsDict[i]["Peer2"] = parts[7]
+            return callsDict
 
         except EOFError:
             print("ERROR callManager:connection closed and no cooked data available")
