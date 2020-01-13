58f004bfd4defd63c53770db1a35a076c3a9fd9b
EgorFokin <egor.fokin2017@gmail.com>
2020-01-13 12:10:01 +0300

    #10

--- AsteriskModule/CallManager.py
+++ AsteriskModule/CallManager.py
@@ -53,18 +53,17 @@ class CallManager:
             callsDict = []
             self.tn.write(b"Action: Command\r\nCommand: sip show channels\r\n\r\n")
             self.tn.read_until(b"Response: Follows\r\n", 10)
-            data = self.tn.read_until(b"END COMMAND", 10).decode().split("\r\n\r\n")
+            data = self.tn.read_until(b"END COMMAND", 10).decode().splitlines(False)
             for i in range(2, len(data)-2):
-                parts = data[i].split()
                 callsDict.append({})
-                callsDict[i]["Peer1"] = parts[0]
-                callsDict[i]["User/ANR"] = parts[1]
-                callsDict[i]["Call_ID"] = parts[2]
-                callsDict[i]["Format"] = parts[3]
-                callsDict[i]["Hold"] = parts[4]
-                callsDict[i]["Last_Message"] = parts[5]
-                callsDict[i]["Expiry"] = parts[6]
-                callsDict[i]["Peer2"] = parts[7]
+                callsDict[i-2]["Peer1"] = data[i][:16].rstrip()
+                callsDict[i-2]["User/ANR"] = data[i][17:33].rstrip()
+                callsDict[i-2]["Call_ID"] = data[i][34:50].rstrip()
+                callsDict[i-2]["Format"] = data[i][51:67].rstrip()
+                callsDict[i-2]["Hold"] = data[i][68:76].rstrip()
+                callsDict[i-2]["Last_Message"] = data[i][77:92].rstrip()
+                callsDict[i-2]["Expiry"] = data[i][93:103].rstrip()
+                callsDict[i-2]["Peer2"] = data[i][104:].rstrip()
             return callsDict
 
         except EOFError:
